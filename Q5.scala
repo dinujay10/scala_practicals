1542b5c12ee2f264c395269eecf17757a4f44e7c
Dinu Jayatissa <111174338+dinujay10@users.noreply.github.com>
2023-07-11 01:42:26 +0530

    Delete Q5.scala
    deleting

--- Q5.scala
+++
@@ -1,7 +0,0 @@
-object Q5 {
-  def addEven(n: Int): Int = {
-    if (n <= 0) return 0 else if(n % 2 == 0) return n + addEven(n - 2)
-    addEven(n - 1)
-  }
-  def main(args: Array[String]): Unit = {println(addEven(7))}
-}
