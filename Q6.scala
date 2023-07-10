91b5d23506626a988e633b6480638c2207a66c15
Dinu Jayatissa <111174338+dinujay10@users.noreply.github.com>
2023-07-11 01:42:46 +0530

    Delete Q6.scala
    deleting

--- Q6.scala
+++
@@ -1,9 +0,0 @@
-object Q6 {
-  def fibonacci(n: Int, a: Int = 0, b: Int = 1): Unit = {
-    if (n > 0) {
-      print(a + " ")
-      fibonacci(n - 1, b, a + b)
-    }
-  }
-  def main(args: Array[String]): Unit = {fibonacci(10)}
-}
